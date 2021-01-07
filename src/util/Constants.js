c5f7e4d951848d5ffc9f905647bf254f36f7064e
Shubham Parihar <shubhamparihar391@gmail.com>
2021-01-08 02:01:44 +0530

    feat(Constants): add reply_settings field

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -20,7 +20,7 @@ export const userFields =
   'created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld';
 
 export const tweetFields =
-  'attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,source,text,withheld';
+  'attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld';
 
 export const mediaFields =
   'duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics,non_public_metrics,organic_metrics,promoted_metrics';
