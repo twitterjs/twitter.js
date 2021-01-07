ec9c9cfea90b545f275b0ed2bdff74f22270cfbd
Shubham Parihar <shubhamparihar391@gmail.com>
2021-01-08 02:03:29 +0530

    feat(StructureBuilder): add pinnedTweet property to User

--- src/util/StructureBuilder.js
+++ src/util/StructureBuilder.js
@@ -2,6 +2,7 @@
 
 import User from '../structures/User.js';
 import Collection from './Collection.js';
+import Tweet from '../structures/Tweet.js';
 
 /**
  * Builds a User structure
@@ -13,11 +14,13 @@ export function userBuilder(client, userData) {
     const usersCollection = new Collection();
     userData.forEach(element => {
       const user = new User(client, element.data);
+      user.pinnedTweet = new Tweet(client, element.includes.tweets[0]);
       usersCollection.set(user.id, user);
     });
     return usersCollection;
   } else {
     const user = new User(client, userData.data);
+    user.pinnedTweet = new Tweet(client, userData.includes.tweets[0]);
     return user;
   }
 }
