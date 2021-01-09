e6e2590bdcc09a7c7b9bda869ddbdb0c8f7a4e03
Shubham Parihar <shubhamparihar391@gmail.com>
2021-01-09 18:05:51 +0530

    feat(Hashtag): add structure for hashtag entity

---
+++ src/structures/Hashtag.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Represents a hastag entity
+ */
+class Hashtag {
+  /**
+   * @param {Object}
+   */
+  constructor(data) {
+    /**
+     * The point at which the hashtag starts
+     * @type {number}
+     */
+    this.start = data.start;
+
+    /**
+     * The point at which the hashtag ends
+     * @param {number}
+     */
+    this.end = data.end;
+
+    /**
+     * The text of the hastag
+     * @type {string}
+     */
+    this.tag = data.tag;
+  }
+}
+
+export default Hashtag;
