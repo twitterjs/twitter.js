85bde1e7b43f1daf7d81051a469c290c1481a50b
Shubham Parihar <shubhamparihar391@gmail.com>
2021-01-09 22:38:42 +0530

    feat(StructureBuilder): add entities to User

--- src/util/StructureBuilder.js
+++ src/util/StructureBuilder.js
@@ -4,6 +4,7 @@ import User from '../structures/User.js';
 import Collection from './Collection.js';
 import Tweet from '../structures/Tweet.js';
 import UserPublicMetrics from '../structures/UserPublicMetrics.js';
+import Entity from '../structures/Entity.js';
 
 /**
  * Builds a User structure
@@ -15,10 +16,11 @@ export function userBuilder(client, userData) {
     const usersCollection = new Collection();
     userData.forEach(element => {
       const user = new User(client, element.data);
-      if (element.includes?.tweets) {
+      if (element.includes?.tweets[0]) {
         user.pinnedTweet = new Tweet(client, element.includes.tweets[0]);
       }
       user.publicMetrics = new UserPublicMetrics(element.data.public_metrics);
+      user.entities = new Entity(element.data.entities);
       usersCollection.set(user.id, user);
     });
     return usersCollection;
@@ -28,6 +30,7 @@ export function userBuilder(client, userData) {
       user.pinnedTweet = new Tweet(client, userData.includes.tweets[0]);
     }
     user.publicMetrics = new UserPublicMetrics(userData.data.public_metrics);
+    user.entities = new Entity(userData.data.entities);
     return user;
   }
 }
