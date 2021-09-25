7ccfae83ee4c527a95838e6934190676096938ee
Shubham Parihar <shubhamparihar391@gmail.com>
2021-09-26 02:03:22 +0530

    fix(RequestHandler): re-connecting to filtered-stream exits process

--- src/rest/RequestHandler.ts
+++ src/rest/RequestHandler.ts
@@ -1,7 +1,8 @@
 import { errors } from 'undici';
 import { HTTPError } from './HTTPError';
-import { TwitterAPIError } from './TwitterAPIError';
+import { setTimeout } from 'timers/promises';
 import { AsyncQueue } from '@sapphire/async-queue';
+import { TwitterAPIError } from './TwitterAPIError';
 import { parseResponse, ClientEvents } from '../util';
 import type { Response } from 'undici';
 import type { APIRequest } from './APIRequest';
@@ -45,42 +46,43 @@ export class RequestHandler {
       }
     }
 
-    if (res && request.isStreaming) return res;
+    if (res.ok) {
+      if (request.isStreaming) return res;
+      const parsedResponse = (await parseResponse(res)) as Record<string, unknown> | ArrayBuffer;
+      if ('errors' in parsedResponse) {
+        /**
+         * Emitted when the raw data of a 200 OK API response contains error along with the requested data.
+         * Use this to debug what fields are missing and why
+         */
+        this.manager.client.emit(ClientEvents.PARTIAL_ERROR, parsedResponse.errors);
 
-    if (res && res.headers) {
-      if (res.ok) {
-        const parsedResponse = (await parseResponse(res)) as Record<string, unknown> | ArrayBuffer;
-        if ('errors' in parsedResponse) {
-          /**
-           * Emitted when the raw data of a 200 OK API response contains error along with the requested data.
-           * Use this to debug what fields are missing and why
-           */
-          this.manager.client.emit(ClientEvents.PARTIAL_ERROR, parsedResponse.errors);
-
-          // Throw error if there is no data field in the response as there is nothing to process. (⚠ not sure this is true for every response, will look this up later)
-          // Currently the thrown error will contain information about the first error only, listen for `partialError` to get the complete error object
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          if ('data' in parsedResponse === false) throw new TwitterAPIError(parsedResponse.errors[0]); // TODO
-        }
-        return parsedResponse;
+        // Throw error if there is no data field in the response as there is nothing to process. (⚠ not sure this is true for every response, will look this up later)
+        // Currently the thrown error will contain information about the first error only, listen for `partialError` to get the complete error object
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if ('data' in parsedResponse === false) throw new TwitterAPIError(parsedResponse.errors[0]); // TODO
       }
-
-      if (res.status >= 400 && res.status < 500) {
-        let apiError: APIProblem;
-        try {
-          apiError = (await parseResponse(res)) as APIProblem;
-        } catch (error) {
-          if (error instanceof errors.UndiciError) {
-            throw new HTTPError(request.method, error.message, error.name, request.path);
-          } else {
-            throw error;
-          }
-        }
-        throw new TwitterAPIError(apiError);
+      return parsedResponse;
+    } else {
+      if (request.isStreaming) {
+        // It seems that there is some kind of latency issue with the filtered tweets stream endpoint,
+        // where the server thinks that the client is connecting with a simultaneous second connection
+        // whereas the client is actually connecting to the stream “again” after disconnecting.
+        // Hence, Wait for an arbitrary 20s period before initiating a new connection to the stream.
+        await setTimeout(20000); // TODO: Remove this once the latency issue has been fixed
+        return this.execute(request);
       }
+      let apiError: APIProblem;
+      try {
+        apiError = (await parseResponse(res)) as APIProblem;
+      } catch (error) {
+        if (error instanceof errors.UndiciError) {
+          throw new HTTPError(request.method, error.message, error.name, request.path);
+        } else {
+          throw error;
+        }
+      }
+      throw new TwitterAPIError(apiError);
     }
-
-    throw new Error('No response');
   }
 }
